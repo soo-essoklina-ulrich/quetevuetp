8faa4a8c869c13ce48b294d7a6e870630795c316
soo-essoklina-ulrich <essoklina.soo@ipnetinstitute.com>
2024-03-26 21:43:43 +0000

    creating component

---
+++ src/components/ApiCategorie.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Data Flask Categorie</h1>
+
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios';
+
+const url = 'http://127.0.0.1:8081/';
+export default {
+  name: 'ApiCategories',
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ApiProjet.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Data Flask Projet</h1>
+    <
+  </div>
+</template>
+
+<script>
+
+import axios from 'axios';
+
+const url = 'http://127.0.0.1:8081/';
+export default {
+  name: 'ApiProjet',
+
+}
+</script>
+
+<style scoped>
+
+</style>
